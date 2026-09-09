6a8dfb9dc2bc52653421b64c74e2067e455cc6f8
LBLaiSiNanHai <lesboyspp43@outlook.com>
2026-09-09 22:01:25 +0800

    misc: 提前注册错误处理器

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@ import {createApp} from 'vue';
 import App from './app';
 import handleError from './Error';
 
-createApp(App).mount('#app');
-
 // eslint-disable-next-line
 function onerror(_a: any, _b: any, _c: any, _d: any, err: Error | string): void {
 	handleError(err ?? '未知错误');
@@ -15,3 +13,5 @@ window.onerror = onerror;
 window.onunhandledrejection = function (e: PromiseRejectionEvent) {
 	onerror(e, '', '', '', e.reason);
 };
+
+createApp(App).mount('#app');
